6bd12d7c46a48d5237d43c5eb4778d49f729c41d
viktor-dejke-nti-johanneberg <94973795+viktor-dejke-nti-johanneberg@users.noreply.github.com>
2022-03-18 18:34:19 +0100

    v.11 fredag
    Börjat med Meny och header för desktop

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //Mobile Nav code
-document.querySelector(".material-icons").addEventListener("click", showMenumobile);
+document.querySelector(".menubutton").addEventListener("click", showMenumobile);
 document.querySelector(".fade-layer").addEventListener("click", showMenumobile);
 document.querySelector(".cross").addEventListener("click", showMenumobile);
 document.querySelector("#contactus").addEventListener("click", showMenumobile);
@@ -9,7 +9,15 @@ function showMenumobile(){
     document.querySelector(".fade-layer").classList.toggle("visible");
 }
 
-function showMenudesktop(){
 
+//Desktop Nav code
+document.querySelector(".menubuttondesk").addEventListener("click", showMenudesktop);
+document.querySelector(".fade-layerdesk").addEventListener("click", showMenudesktop);
+document.querySelector(".crossdesk").addEventListener("click", showMenudesktop);
+document.querySelector("#contactusdesk").addEventListener("click", showMenudesktop);
+
+function showMenudesktop(){
+    document.querySelector(".navdesktop").classList.toggle("showdesktop");
+    document.querySelector(".fade-layerdesk").classList.toggle("visibledesktop");
 }
 
