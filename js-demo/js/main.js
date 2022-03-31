cc06e8937a185d8ec137c28cf89b8dfdee79172d
viktor-dejke-nti-johanneberg <94973795+viktor-dejke-nti-johanneberg@users.noreply.github.com>
2022-03-31 21:07:41 +0200

    Fixat så att logga passar in på A nivå för photoshop, ändrat så att nya loggan är överallt

--- js-demo/js/main.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".menu-button").addEventListener("click", showMenu);
-document.querySelector(".fade-layer").addEventListener("click", showMenu);
-
-function showMenu(){
-    document.querySelector("nav.menu").classList.toggle("show");
-    document.querySelector(".fade-layer").classList.toggle("visible");
-}
